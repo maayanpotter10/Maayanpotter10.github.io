d9ca7ed7a131f217d1084cc058f05b8913fc0af7
Maayanpotter10 <Maayan.potter.10@gmail.com>
2025-12-14 11:31:26 -0500

    kyctf

--- index.js
+++ index.js
@@ -8,5 +8,14 @@ let stylish = document.getElementById("stylish");
 console.log(stylish);
 stylish.addEventListener("click",function() { 
     console.log("click")
-    stylish.style.backgroundColor="#00f"
+    stylish.style.backgroundColor=makeRandomColor()
+    stylish.style.color=makeRandomColor();
 })
+function makeRandomColor () {
+    let r=Math.floor(Math.random() * 256);
+    let g=Math.floor(Math.random() * 256);
+    let b=Math.floor(Math.random() * 256);
+
+    let color = `rgb(${r}, ${g},${b})`
+    return color
+}
